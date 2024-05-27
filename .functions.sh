f0b1a8934d927c96ea43973693468c1b848cb080
Thisaru Guruge <thisaru.guruge@gmail.com>
2024-05-28 00:23:32 +0530

    Intial commit

---
+++ .functions.sh
@@ -0,0 +1,53 @@
+compress_files () {
+    tar -czvf "$1.tar.gz" $1
+}
+
+extract () {
+     if [ -f $1 ] ; then
+         case $1 in
+             *.tar.bz2)   tar xjf $1     ;;
+             *.tar.gz)    tar xzf $1     ;;
+             *.bz2)       bunzip2 $1     ;;
+             *.rar)       unrar e $1     ;;
+             *.gz)        gunzip $1      ;;
+             *.tar)       tar xf $1      ;;
+             *.tbz2)      tar xjf $1     ;;
+             *.tgz)       tar xzf $1     ;;
+             *.zip)       unzip $1       ;;
+             *.Z)         uncompress $1  ;;
+             *.7z)        7z x $1        ;;
+             *)     echo "'$1' cannot be extracted via extract()" ;;
+         esac
+     else
+         echo "'$1' is not a valid file"
+     fi
+}
+
+checkPort() {
+    lsof -i:$1
+}
+
+kill_by_port() {
+    re='^[0-9]{4}$'
+    if ! [ $1 -lt 65534 ] && [ $1 -gt 1000 ]
+    then
+        echo "Invalid input: $1. Enter a valid port number. (Numbers less than 1000 are considered invalid)"
+    else
+        apps="$(lsof -i:$1)"
+        if [ -z $apps  ]
+        then
+            echo "No apps using the port $1"
+        else
+            echo "Apps found: "
+            echo "$apps"
+            #read "resonse?Do you want to kill these ? (y/N): "
+            #if [[ $response =~ '^[yY]$' ]]
+            #then
+                kill -9 $(lsof -t -i:$1)
+                echo "Killed Apps"
+            #else
+            #    echo "Quit"
+            #fi
+        fi
+    fi
+}
