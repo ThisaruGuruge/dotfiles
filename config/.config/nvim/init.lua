959f40950a399e067a4ff5bcb743482fbb795d00
Thisaru Guruge <thisaru@wso2.com>
2026-02-02 14:53:51 +0530

    fix(nvim): fix nvim treesitter and telescope issues

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -1,2 +1,7 @@
+-- Compatibility shim for Neovim 0.11+ (ft_to_lang was removed)
+if not vim.treesitter.language.ft_to_lang then
+	vim.treesitter.language.ft_to_lang = vim.treesitter.language.get_lang
+end
+
 require("thisarug")
 require("config.lazy")
