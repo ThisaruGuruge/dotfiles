27a5ebf725d42ad748a526cd18ec73dbd93450df
Thisaru Guruge <thisaru@wso2.com>
2026-05-08 20:15:50 +0530

    feat(nvim): implement custom live grep

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,15 +3,67 @@ return {
         "nvim-telescope/telescope.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope-live-grep-args.nvim",
+{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
         },
         cmd = "Telescope",
+        keys = {
+            { "<leader>en", function()
+                require('telescope.builtin').find_files { cwd = vim.fn.stdpath("config") }
+            end, desc = "Find Neovim config files" },
+            { "<leader>ep", function()
+                require('telescope.builtin').find_files { cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") }
+            end, desc = "Find plugin files" },
+            { "<leader>fg", desc = "Live Grep (multi)" },
+        },
         config = function()
-            local telescope = require("telescope")
-            local actions = require("telescope.actions")
-            local lga_actions = require("telescope-live-grep-args.actions")
+            local pickers = require("telescope.pickers")
+            local finders = require("telescope.finders")
+            local make_entry = require("telescope.make_entry")
+            local conf = require("telescope.config").values
 
-            telescope.setup({
+            local live_multigrep = function(opts)
+                opts = opts or {}
+                opts.cwd = opts.cwd or vim.uv.cwd()
+                local finder = finders.new_async_job {
+                    command_generator = function(prompt)
+                        if not prompt or prompt == "" then return nil end
+                        local pieces = vim.split(prompt, "  ")
+                        local args = { "rg" }
+                        if pieces[1] then
+                            table.insert(args, "-e")
+                            table.insert(args, pieces[1])
+                        end
+                        if pieces[2] and pieces[2] ~= "" then
+                            table.insert(args, "--type")
+                            table.insert(args, pieces[2])
+                        end
+                        if pieces[3] then
+                            table.insert(args, "-g")
+                            table.insert(args, pieces[3])
+                        end
+                        table.insert(args, { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" })
+                        return vim.iter(args):flatten():totable()
+                    end,
+                    entry_maker = make_entry.gen_from_vimgrep(opts),
+                    cwd = opts.cwd,
+                }
+                pickers.new(opts, {
+                    debounce = 100,
+                    prompt_title = "Multi Grep",
+                    finder = finder,
+                    previewer = conf.grep_previewer(opts),
+                    sorter = require("telescope.sorters").empty(),
+                }):find()
+            end
+
+            vim.keymap.set("n", "<leader>fg", live_multigrep)
+
+            require('telescope').setup({
+                pickers = {
+                    find_files = {
+                        theme = "ivy"
+                    }
+                },
                 defaults = {
                     mappings = {
                         i = {
@@ -19,7 +71,7 @@ return {
                             ["<C-d>"] = false,
                         },
                         n = {
-                            ["q"] = actions.close,
+                            ["q"] = require('telescope.actions').close,
                         },
                     },
                     file_ignore_patterns = { "node_modules", ".git/", "%.lock" },
@@ -30,20 +82,10 @@ return {
                     },
                 },
                 extensions = {
-                    live_grep_args = {
-                        auto_quoting = true,
-                        mappings = {
-                            i = {
-                                ["<C-k>"] = lga_actions.quote_prompt(),
-                                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-                                ["<C-t>"] = lga_actions.quote_prompt({ postfix = " -t " }),
-                            },
-                        },
-                    },
-                },
+                    fzf = {}
+                }
             })
-
-            telescope.load_extension("live_grep_args")
+            require('telescope').load_extension('fzf')
         end,
     },
 }
