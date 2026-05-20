b304ba398904b959eb7420fd1b8e70dd130e6ca4
Thisaru Guruge <thisaru@wso2.com>
2026-05-20 23:36:14 +0530

    chore(nvim): update scroll length and add Sinhala

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -12,6 +12,7 @@ config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font_with_fallback({
 	"FiraCode Nerd Font",
 	"JetBrains Mono",
+	"Noto Sans Sinhala",
 	"Menlo",
 })
 config.font_size = 14.0
